648a34f6456e062f82875a0198eab387e0c40863
Sidney Owallah <sidneyowallah@gmail.com>
2020-09-02 07:36:49 -0500

    Implemented rabbitmq message consumer

--- src/main/java/com/lakehue/learn/LearnApplication.java
+++ src/main/java/com/lakehue/learn/LearnApplication.java
@@ -6,15 +6,59 @@ import com.lakehue.learn.data.entity.Room;
 import com.lakehue.learn.data.repository.GuestRepository;
 import com.lakehue.learn.data.repository.ReservationRepository;
 import com.lakehue.learn.data.repository.RoomRepository;
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearnApplication {
+	@Value("${amqp.queue.name}")
+	private String queueName;
+
+	@Value("${amqp.exchange.name}")
+	private String exchangeName;
+
+	@Bean
+	public Queue queue(){
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	public TopicExchange topicExchange(){
+		return new TopicExchange(exchangeName);
+	}
+
+	@Bean
+	public Binding binding(Queue queue, TopicExchange topicExchange){
+		return BindingBuilder.bind(queue).to(topicExchange).with(queueName);
+	}
+
+	@Bean
+	public MessageListenerAdapter listenerAdapter (RoomCleanerProcessor processor){
+		return new MessageListenerAdapter(processor, "receiveMessage");
+	}
+
+	@Bean
+	public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter){
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
